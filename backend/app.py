fee61e1098d14f868aeb10e98b7295d81d13528f
everupward248 <everupward248@gmail.com>
2025-10-11 13:26:57 -0500

    db connection made, to make api endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, session, redirect, flash, request, url_for
+import functions as func
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     if request.method == "GET":
+        try:
+            
+
+
         return "hello"
 
 
--- backend/functions/db_conn.py
+++ backend/functions/db_conn.py
@@ -4,6 +4,11 @@ from config import config
 def connect():
     try:
         connection = None
+        # import params from config file
         params = config()
+        # unpack the parameters from the .ini file
+        connection = psycopg.connect(**params)
+        print("connection successful...")
+        return connection
     except(Exception, psycopg.DatabaseError) as error:
         print(error)
