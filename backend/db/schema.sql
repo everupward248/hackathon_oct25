0a4d3f753b8714769093a67935cf81524edc8e71
everupward248 <everupward248@gmail.com>
2025-10-11 12:04:47 -0500

    created schema for db tables. to import data and add connection logic

---
+++ backend/db/schema.sql
@@ -0,0 +1,45 @@
+-- separate into separate tables to reduce redundancy 
+CREATE TABLE IF NOT EXISTS "jobs" (
+    "id" SERIAL,
+    "job_post_id" INTEGER UNIQUE NOT NULL,
+    "job_title" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "created_date" DATE NOT NULL,
+    "start_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "cig_sagc" BOOLEAN NOT NULL,
+    "work_type" TEXT NOT NULL,
+    "employer" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "occupation" TEXT NOT NULL,
+    "sub_industry" TEXT NOT NULL,
+    "industry" TEXT NOT NULL,
+    PRIMARY KEY("id")
+    
+);
+
+CREATE TABLE IF NOT EXISTS "experiences" (
+    "id" SERIAL,
+    "job_id" INT NOT NULL,
+    "job_post_id" INTEGER UNIQUE NOT NULL,
+    "required_education_level" TEXT NOT NULL,
+    "years_experience" TEXT NOT NULL,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("job_id") REFERENCES "jobs"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "renumerations" (
+    "id" SERIAL,
+    "job_id" INT NOT NULL,
+    "job_post_id" INTEGER UNIQUE NOT NULL,
+    "hours_per_week" DOUBLE PRECISION NOT NULL, 
+    "currency" TEXT NOT NULL, 
+    "pay_frequency" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "min_salary" DOUBLE PRECISION NOT NULL,
+    "max_salary" DOUBLE PRECISION NOT NULL,
+    "annualised_min" DOUBLE PRECISION NOT NULL,
+    "annualised_max" DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("job_id") REFERENCES "jobs"("id")
+);
