c3a7bbe4f47066429fb043919139958a4c75ca33
everupward248 <everupward248@gmail.com>
2025-10-14 20:38:34 -0500

    admin

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from backend.functions import db_conn
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
@@ -13,4 +15,4 @@ def index():
             print(e)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
